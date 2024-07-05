689f01e31685f8b8b936341260321d7e0431bda4
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-07-05 21:58:22 +0100

    proj init

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,72 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  title: 'unlonely',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+})
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+ 
+  const farcasterId = c.frameData?.fid
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            ? `Hello: ${farcasterId}`
+            : 'Subscribe to join out live stream list!'}
+        </div>
+      </div>
+    ),
+    intents: [
+      
+      <Button value="subscribe">Subscribe</Button>,
+      status === 'response' && <Button.Reset>Unsubscribe</Button.Reset>,
+    ],
+  })
+})
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
